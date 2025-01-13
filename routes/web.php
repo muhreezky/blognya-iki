b2e7d2aa44956789a60e811cb6c6b8b97c29e489
Rizkiansyah <muhammadrizkiansyah60@gmail.com>
2025-01-13 21:42:23 +0700

    chore: init laravel filament

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('storage/{name}', function ($name) {
+    $disk = Storage::disk('public');
+    $exists = $disk->exists($name);
+    if (!$exists) return response()->json(['message' => 'File not found'], 404);
+    $file = $disk->get($name);
+    return response()->file($file, [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, OPTIONS',
+        'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+    ]);
+})->where('name', '.*');
