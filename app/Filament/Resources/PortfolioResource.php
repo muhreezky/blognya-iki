ae966ea86efc8b37fc8ea5cf85e12c622c209b2e
Rizkiansyah <muhammadrizkiansyah60@gmail.com>
2025-03-02 09:08:23 +0700

    feat: add crud feature for portfolio in admin panel

---
+++ app/Filament/Resources/PortfolioResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PortfolioResource\Pages;
+use App\Filament\Resources\PortfolioResource\RelationManagers;
+use App\Models\Portfolio;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PortfolioResource extends Resource
+{
+    protected static ?string $model = Portfolio::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()->columnSpanFull()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('thumbnail')
+                    ->required()->columnSpanFull()
+                    ->image()->imageEditor(),
+                Forms\Components\RichEditor::make('description')
+                    ->required()->maxLength(65535)
+                    ->columnSpanFull()->disableToolbarButtons(['attachFiles']),
+                Forms\Components\TextInput::make('url')
+                    ->required()->columnSpanFull()
+                    ->maxLength(255)->placeholder('https://example.com/....'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('url')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('thumbnail')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPortfolios::route('/'),
+            'create' => Pages\CreatePortfolio::route('/create'),
+            'edit' => Pages\EditPortfolio::route('/{record}/edit'),
+        ];
+    }
+}
